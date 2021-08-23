d169d24640ac715b3e383a4f1f0c9fd2397a857e
Bennett Critchley <bennett.critchley@hotmail.com>
2021-08-22 23:19:08 -0600

    Adding to js file(Random card button now works fully with a fetch and alot of hard work lol)

--- index.js
+++ index.js
@@ -8,14 +8,41 @@ function watchPriceChecker() {
 
 // fetches the random card from the api then sends the data to mainPageDisplayRandom
 function mainPageRandomFetch() {
-  console.log('Loading Random Card...')
-  mainPageDisplayRandom();
+  const randomCardUrl = "https://api.scryfall.com/cards/random?digital=false";
+  console.log('Loading Random Card...');
+
+  fetch(randomCardUrl)
+    .then(response => response.json())
+    .then(data => mainPageDisplayRandom(data));
 }
 
 
 
 // Takes the fetch data (from random) and displays it aswell as removes the hidden class
-function mainPageDisplayRandom() {
+function mainPageDisplayRandom(data) {
+  $(".random-card-area").empty();
+  let randomCardName = data.name;
+  let randomCardImg = data.image_uris.small;
+  let randomAbilitys = data.oracle_text;
+  let randomPriceToFind = data.prices.usd;
+  let randomFoilPriceToFind = data.prices.usd_foil;
+  let randomRank = data.edhrec_rank;
+  if(randomPriceToFind === null){
+    randomPriceToFind = "Price Not Available. :("
+  };
+  $(".random-card-area").append(`
+          <div class="mainPageCard">
+            <div class="mainPageCardImg">
+              <img src="${randomCardImg}">
+            </div>
+            <div class="mainPageCardInfo">
+              <h4>Name: ${randomCardName}</h4>
+              <h5>Price: ${randomPriceToFind}</h5>
+              <h5>Foil price: ${randomFoilPriceToFind}</h5>
+              <p>Abilitys: ${randomAbilitys}</p>
+              <p>EDHRec Rank: ${randomRank}</p>
+            </div>
+          </div>`)
   $('div').removeClass('hidden')
 }
 
